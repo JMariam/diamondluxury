6b423c09470de0531faf0ab0ea7c6c61121a23a1
Emjay <107817196+JMariam@users.noreply.github.com>
2023-09-21 17:10:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <Router>
       <Header/>
     <Routes>
-      <Route path='/diamondluxury' element={<Home/>}/>
+      <Route path='/diamondluxury' exact element={<Home/>}/>
       <Route path='' element={<Err404/>}/>
     </Routes>
     <Footer/>
